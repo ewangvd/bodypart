8eb0f50d75e70e947247187b354d2704f10cad6e
Ewan Gillies <57173905+ewangvd@users.noreply.github.com>
2023-04-06 10:09:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./styles.css";
+import { BodyComponent } from "reactjs-human-body";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <BodyComponent
+          partsInput={{
+            head: { show: true },
+            left_shoulder: { show: true },
+            right_shoulder: { show: true },
+            left_arm: { show: true },
+            right_arm: { show: true },
+            chest: { show: true },
+            stomach: { show: true },
+            left_leg: { show: true },
+            right_leg: { show: true },
+            left_hand: { show: true },
+            right_hand: { show: true },
+            left_foot: { show: true },
+            right_foot: { show: true }
+          }}
+        />
+      </div>
+    </div>
+  );
+}
